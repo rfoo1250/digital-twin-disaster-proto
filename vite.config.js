0e391cb107d36926322be1f548f1b21d0fee9956
rfoo1250 <rfoo1@asu.edu>
2025-11-18 13:47:16 -0700

    UPDATE - using Vite proxy, install @turf/turf (correct one)

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,14 @@ import { defineConfig } from 'vite'
 export default defineConfig({
   server: {
     port: 5656,
-    open: true
+    open: true,
+    proxy: {
+      '/api': {
+        target: 'http://127.0.0.1:5000',
+        changeOrigin: true,
+        secure: false,
+      },
+    },
   },
   build: {
     outDir: 'distb'     // default output folder
